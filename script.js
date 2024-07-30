c4f581fd63c3948d28c24c7bce80572ad5b00e7a
Tara <z.bahrampour1@gmail.com>
2024-07-30 10:25:33 +0330

    adding starting project

---
+++ script.js
@@ -0,0 +1,29 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+// alert(multiply(10, 5))
+
+// function checkDriverAge(age) {
+//   if (Number(age) < 18) {
+//     alert("Sorry, you are too yound to drive this car. Powering off");
+//   } else if (Number(age) > 18) {
+//     alert("Powering On. Enjoy the ride!");
+//   } else if (Number(age) === 18) {
+//     alert("Congratulations on your first year of driving. Enjoy the ride!");
+//   }
+// }
+
+// a = prompt('please enter your age= ')
+// checkDriverAge(a)
+
+todo = [
+  'hellooo',
+  'good morning',
+  'good evening',
+  'have a good day'
+]
+
+todo.array.forEach(element => {
+
+});
